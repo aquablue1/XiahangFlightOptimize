b635d5a9b24c875c39ccf5f13c23928117895730
aquablue1 <aquablue1@users.noreply.github.com>
2017-07-24 17:05:49 +0800

    jobline functions used.

--- src/ListAndDictGenerate.py
+++ src/ListAndDictGenerate.py
@@ -18,7 +18,7 @@ def generate_accident():
 
 
 def generate_airline_string(airline_set):
-    airline_string_dict = []
+    airline_string_dict = {}
     for planeID in list(set([line.LinePlaneID for line in airline_set])):
         airline_string_dict[planeID] = planeJobLine(planeID)
     for airline in airline_set:
@@ -30,7 +30,7 @@ def generate_airline_string(airline_set):
 def generate_directInfluenced_airline(airline_set, accident_set):
     def is_influenced(airline, accident_set):
         if timePeriod.is_time_intersect(airline.LineFlyPeriod,
-                                        accident_set[0].TyphoonForbiddenLand.forbiddenRuleTimePeriod) \
+                                        accident_set[0].TyphoonForbiddenLand.ForbiddenRuleTimePeriod) \
             and (airline.LineDepartureAirport in [typhoon.TyphoonAirportID for typhoon in accident_set]
                      or airline.LineLandAirport in [typhoon.TyphoonAirportID for typhoon in accident_set]):
             return True
@@ -39,7 +39,7 @@ def generate_directInfluenced_airline(airline_set, accident_set):
     influenced_airline_set = []
     influenced_plane_set = []
     for airline in airline_set:
-        if is_influenced(airline_set, accident_set):
+        if is_influenced(airline, accident_set):
             influenced_airline_set.append(airline)
             influenced_plane_set.append(airline.LinePlaneID)
             airline.LineIsInfluenced = 1
@@ -50,8 +50,10 @@ def generate_directInfluenced_airline(airline_set, accident_set):
 def generate_plane_set(airline_set):
     return list(set([line.LinePlaneID for line in airline_set]))
 
+
 def generate_indirectInfluenced_airline(directInfluenced_airline_set, airline_string_dict):
     indirectInfluenced_airline_set = []
+    end_append, mid_reset = 0, 0
     for planeID in airline_string_dict.keys():
         lineset = airline_string_dict[planeID].planeFlightList
         start, end = 0, len(lineset)-1
@@ -64,18 +66,30 @@ def generate_indirectInfluenced_airline(directInfluenced_airline_set, airline_st
         for j in range(len(lineset)):
             if lineset[end - j].LineIsInfluenced == 1:
                 is_influenced = True
-                end = j
+                end = end - j
                 break
         if is_influenced:
-            airline_string_dict[planeID].planeInfluencedRange = (start, end)
+            airline_string_dict[planeID].planeInfluencedRange = [start, end]
             for index in range(start, end+1):
                 if lineset[index].LineIsInfluenced != 1:
                     lineset[index].LineIsInfluenced = 2
+                    mid_reset += 1
                     indirectInfluenced_airline_set.append(lineset[index])
-        while True:
-            if end == len(lineset)-1:
-                break
-
+            while True:
+                if airline_string_dict[planeID].planeInfluencedRange[1] >= len(lineset)-1:
+                    break
+                if D.flight_timecost(D.planeID_totype(lineset[start].LinePlaneID),
+                                     lineset[start].LineDepartureAirport,
+                                     lineset[end].LineLandAirport) is not False:
+                    # print(D.planeID_totype(lineset[start].LinePlaneID), lineset[start].LineDepartureAirport, lineset[end].LineLandAirport)
+                    break
+                end += 1
+                airline_string_dict[planeID].planeInfluencedRange[1] = end
+                lineset[end].LineIsInfluenced = 2
+                end_append += 1
+                indirectInfluenced_airline_set.append(lineset[end])
+    # print(mid_reset, end_append)
+    return indirectInfluenced_airline_set
 
 
 def generate_influenced_airline_string(airline_string):
@@ -83,91 +97,70 @@ def generate_influenced_airline_string(airline_string):
         return airline_string.planeFlightList[airline_string.planeInfluencedRange[0]: airline_string.planeInfluencedRange[1]+1]
 
 
-
-
 if __name__ == '__main__':
-    airline_head, airline_set = form_airlines()
-    typhoon_head, typhoon_set = form_typhoon_accidents()
+    airline_head, airline_set = generate_airline()
+    typhoon_head, typhoon_set = generate_accident()
 
-    influenced_airline_set = []
-    influenced_flight_set = []
-    print(typhoon_set[1].TyphoonForbiddenStop)
-    for airline in airline_set:
-        if timePeriod.is_time_intersect(airline.LineFlyPeriod,
-                                        typhoon_set[0].TyphoonForbiddenLand.ForbiddenRuleTimePeriod) \
-                and (airline.LineDepartureAirport in [typhoon.TyphoonAirportID for typhoon in typhoon_set]
-                     or airline.LineLandAirport in [typhoon.TyphoonAirportID for typhoon in typhoon_set]):
-            influenced_airline_set.append(airline)
-            airline.LineIsInfluenced=1
-            influenced_flight_set.append(airline.LinePlaneID)
-    influenced_flight_set = list(set(influenced_flight_set))
-    print(len([line for line in airline_set if line.LineIsInfluenced==1]), '\n ----')
+    airline_string_dict = generate_airline_string(airline_set)
 
-    plane_jobline_dict = {}
-    for planeID in list(set([line.LinePlaneID for line in airline_set])):
-        plane_jobline_dict[planeID] = planeJobLine(planeID)
 
-    for line in influenced_airline_set:
-        plane_jobline_dict[line.LinePlaneID].insert_airline(line)
+    directInfluenced_airline_set, influenced_plane_set = generate_directInfluenced_airline(airline_set, typhoon_set)
 
-    for line in plane_jobline_dict["3"].planeFlightList:
-        print(line)
+    indirectInfluenced_airline_set = generate_indirectInfluenced_airline(directInfluenced_airline_set, airline_string_dict)
 
-    with open(cpara.PATH_OUTPUT, 'w', newline='') as f:
-        f_csv = csv.writer(f)
-        for line in airline_set:
-            if line.LineIsInfluenced ==0:
-                row = [line.LineID,
-                       line.LineDepartureAirport,
-                       line.LineLandAirport,
-                       line.LineFlyPeriod.start.strftime(cpara.OUTPUT_DATETIME_FORM),
-                       line.LineFlyPeriod.end.strftime(cpara.OUTPUT_DATETIME_FORM),
-                       line.LinePlaneID,
-                       0,0,0
-                       ]
-            else:
-                row = [line.LineID,
-                       line.LineDepartureAirport,
-                       line.LineLandAirport,
-                       line.LineFlyPeriod.start.strftime(cpara.OUTPUT_DATETIME_FORM),
-                       line.LineFlyPeriod.end.strftime(cpara.OUTPUT_DATETIME_FORM),
-                       line.LinePlaneID,
-                       1,0,0
-                       ]
-            f_csv.writerow(row)
+    print(len(indirectInfluenced_airline_set))
 
-    print("len of plane_jobline_dict.keys() is ", len(plane_jobline_dict.keys()))
-    with open(cpara.PATH_OUTPUT, 'a', newline='') as f:
-        f_csv = csv.writer(f)
-        start = 9001
-        for plane in plane_jobline_dict.keys():
-            if len(plane_jobline_dict[plane].planeFlightList) == 0:
-                continue
-            if plane_jobline_dict[plane].planeFlightList[0].LineDepartureAirport == \
-                plane_jobline_dict[plane].planeFlightList[-1].LineLandAirport:
-                # print("no need to transfer:", plane_jobline_dict[plane].planeID)
-                continue
-            if D.flight_timecost(D.planeID_totype(plane_jobline_dict[plane].planeFlightList[0].LinePlaneID),
-                                      plane_jobline_dict[plane].planeFlightList[0].LineDepartureAirport,
-                                      plane_jobline_dict[plane].planeFlightList[-1].LineLandAirport) is False:
-                print("unable to transfer: ", plane_jobline_dict[plane].planeID)
-                continue
-            # print(D.planeID_totype(plane_jobline_dict[plane].planeFlightList[0].LinePlaneID))
-            # print(plane_jobline_dict[plane].planeFlightList[0].LineDepartureAirport)
-            # print(plane_jobline_dict[plane].planeFlightList[-1].LineLandAirport, '\n---')
-            row = [start,
-                   plane_jobline_dict[plane].planeFlightList[0].LineDepartureAirport,
-                   plane_jobline_dict[plane].planeFlightList[-1].LineLandAirport,
-                   plane_jobline_dict[plane].planeFlightList[0].LineFlyPeriod.start.strftime(cpara.OUTPUT_DATETIME_FORM),
-                   (plane_jobline_dict[plane].planeFlightList[0].LineFlyPeriod.start+
-                    D.flight_timecost(D.planeID_totype(plane_jobline_dict[plane].planeFlightList[0].LinePlaneID),
-                                      plane_jobline_dict[plane].planeFlightList[0].LineDepartureAirport,
-                                      plane_jobline_dict[plane].planeFlightList[-1].LineLandAirport,
-                    )).strftime(cpara.OUTPUT_DATETIME_FORM),
-                   plane_jobline_dict[plane].planeID,
-                   0,0,1
-            ]
-            start += 1
-            f_csv.writerow(row)
+    """    
+    for planeid in airline_string_dict.keys():
+        print(planeid, end=" : ")
+        for line in airline_string_dict[planeid].planeFlightList:
+            print(line.LineIsInfluenced, end=" -> ")
+        print("")
+    """
 
-    plane_jobline_dict["70"].show_planejobline_detail()
+    for plane in airline_string_dict.keys():
+        cow_count = 9001
+        lineset = airline_string_dict[plane].planeFlightList
+        with open(cpara.PATH_OUTPUT, 'a', newline='') as f:
+            f_csv = csv.writer(f)
+            for line in lineset:
+                if line.LineIsInfluenced == 0:
+                    row = [line.LineID,
+                           line.LineDepartureAirport,
+                           line.LineLandAirport,
+                           line.LineFlyPeriod.start.strftime(cpara.OUTPUT_DATETIME_FORM),
+                           line.LineFlyPeriod.end.strftime(cpara.OUTPUT_DATETIME_FORM),
+                           line.LinePlaneID,
+                           0, 0, 0]
+                else:
+                    row = [line.LineID,
+                           line.LineDepartureAirport,
+                           line.LineLandAirport,
+                           line.LineFlyPeriod.start.strftime(cpara.OUTPUT_DATETIME_FORM),
+                           line.LineFlyPeriod.end.strftime(cpara.OUTPUT_DATETIME_FORM),
+                           line.LinePlaneID,
+                           1, 0, 0]
+                f_csv.writerow(row)
+
+            if airline_string_dict[plane].planeInfluencedRange is not None:
+                start = airline_string_dict[plane].planeInfluencedRange[0]
+                end = airline_string_dict[plane].planeInfluencedRange[1]
+                if lineset[start].LineDepartureAirport != lineset[end].LineLandAirport:
+                    row = [cow_count,
+                           lineset[start].LineDepartureAirport,
+                           lineset[end].LineLandAirport,
+                           lineset[start].LineFlyPeriod.start.strftime(
+                               cpara.OUTPUT_DATETIME_FORM),
+                           (lineset[start].LineFlyPeriod.start +
+                            D.flight_timecost(
+                                D.planeID_totype(lineset[start].LinePlaneID),
+                                lineset[start].LineDepartureAirport,
+                                lineset[end].LineLandAirport,
+                                )).strftime(cpara.OUTPUT_DATETIME_FORM),
+                           airline_string_dict[plane].planeID,
+                           0, 0, 1
+                           ]
+                    cow_count += 1
+                    f_csv.writerow(row)
+
+
